7f237d0d9370330847146a8bf1767bf5d0588cf4
cksun@nersc <sunck@pku.edu.cn>
2016-06-22 06:04:50 -0700

    save2nc.py into boututils, for saving data into NetCDF file

---
+++ Python/save2nc.py
@@ -0,0 +1,22 @@
+from boututils import DataFile
+
+def save2nc(file_name,**var):
+  """save2nc(file_name, varName=var, ...)
+    save variables to NetCDF file. 
+
+    'file_name' is the exported file name, and the suffix ".nc" will be append automatically.
+    'varName' is the name saved in exported files,
+    'var' is the variable saved. varName and var do NOT need the quotation mark.
+
+    E.g. save2nc('file',t=t,x=psi,P0=P0_origin) will save variables t as t, psi as x, P0_origin as P0 into a NetCDF file named 'file.nc'.
+  """
+  if file_name[-3:] != '.nc':
+    file_name += '.nc'
+  f = DataFile(file_name, write=True, create=True)
+  for v in var:
+    try:
+      varg = var[v]
+      f.write(v,varg)
+    except:
+      print 'check the source code "save2nv.py" for more infomation'
+  f.close()
