c3b1c1c600c5168eaa2d8c1bc408ccb9f18aaded
bashInWin10 <sck_pku@foxmail.com>
2016-09-19 01:29:21 +0800

    change timing sentences

--- Python/rotate_img_parallel.py
+++ Python/rotate_img_parallel.py
@@ -3,7 +3,6 @@ from glob import glob
 from PIL import Image
 from time import time
 
-startTime = time()
 ImgLst = glob('/Users/PLS/Downloads/Bab2/*')
 def rotateImage(ImgNm):
     try:
@@ -17,11 +16,12 @@ def rotateImage(ImgNm):
     except IOError:
         pass
 
-from  multiprocessing import Pool
 if __name__ == '__main__':
+    from  multiprocessing import Pool
+    startTime = time()
     pool = Pool(processes=4)
     pool.map(rotateImage, ImgLst)
 
-endTime = time()
-print "%d images"%len(ImgLst)
-print "execute time: %.2fs"%(endTime-startTime)
+    endTime = time()
+    print "%d images"%len(ImgLst)
+    print "execute time: %.2fs"%(endTime-startTime)
